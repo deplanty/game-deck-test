915b1be624491e2a8cbd60a5ec778db19e8fd1d4
deplanty <yvon.deplante@gmail.com>
2024-05-01 18:10:42 +0200

    Create a toolkit to manage text in the console

---
+++ src/tui/widget.py
@@ -0,0 +1,176 @@
+import textwrap
+
+import curses
+
+
+class Widget:
+    """
+    The base class for all widgets.
+
+    Args:
+        parent (Widget): The parent widget.
+    """
+
+    def __init__(self, parent):
+        self.parent = parent
+        self.children = list()
+
+        # Grid position and size
+        self.row = 0
+        self.column = 0
+        self.height = 1
+
+        # At the end of this instanciation, add this widget as a child
+        if parent is not None:
+            self.parent.add_child(self)
+
+    def __str__(self) -> str:
+        parents = [self.__class__.__name__]
+        parent = self.parent
+        while parent is not None:
+            parents.insert(0, parent.__class__.__name__)
+            parent = parent.parent
+        return ".".join(parents)
+
+    # Properties
+
+    @property
+    def pos_row(self) -> int:
+        """Returns the top row position of the widget."""
+
+        if self.parent is None:
+            return 0
+        else:
+            return self.parent.pos_row + self.row
+
+    @property
+    def pos_column(self) -> int:
+        """Returns the left column position of the widget."""
+        if self.parent is None:
+            return 0
+        else:
+            return self.parent.pos_column + self.column
+
+    # Methods
+
+    def update(self):
+        """How the widget displays information."""
+
+        raise NotImplementedError()
+
+    def _update(self):
+        """Hiden function to update all the children widgets and this widget."""
+
+        for child in self.children:
+            child.update()
+            child._update()
+        self.update()
+
+    def add_child(self, child):
+        """
+        Add a child to this widget
+
+        Args:
+            child (Widget): The child widget.
+        """
+
+        self.children.append(child)
+        self.children.sort(key=lambda x: x.row)
+
+    def grid(self, row:int, column:int=0, height:int=1):
+        """
+        How should be displayed the widget with its siblings.
+
+        Args:
+            row (int): The grid row
+            column (int): The grid column. Defaults to 0.
+            height (int): The height of the widget. Defaults to 1.
+
+        FIXME: Manage when several widgets are on the same "grid cell".
+        """
+
+        self.row = row
+        self.column = column
+        self.height = height
+
+    def grid_get_row_position(self, row:int) -> int:
+        """
+        Returns the absolute row position for ont of its child from a griven
+        grid row.
+
+        Args:
+            row (int): The grid row.
+
+        Returns:
+            int: The absolute position.
+        """
+
+        row_position = self.pos_row
+        for i in range(row):
+            row_position += self.children[i].height
+        return row_position
+
+
+class Tui(Widget):
+    def __init__(self):
+        super().__init__(None)
+        self.scr = curses.initscr()
+        self.children = list()
+
+    # Methods
+
+    def update(self):
+        for child in self.children:
+            child._update()
+        self.scr.refresh()
+
+
+class Frame(Widget):
+    """
+    A simple widget use to contains other widgets
+
+    Args:
+        parent (Widget): The parent widget
+    """
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+    def update(self):
+        pass
+
+
+class Label(Widget):
+    """
+    A simple widget that displays some text.
+
+    Args:
+        parent (Widget): The parent widget
+    """
+
+    def __init__(self, parent, text:str=""):
+        super().__init__(parent)
+        self._text = text
+
+    # Properties
+
+    @property
+    def text(self) -> str:
+        return self._text
+
+    @text.setter
+    def text(self, value:str):
+        self._text = value
+
+    # Methods
+
+    def update(self):
+        x = self.pos_column
+        y = self.parent.grid_get_row_position(self.row)
+        width = 50
+        height = self.height
+
+        text = textwrap.wrap(self._text, width)
+        nlines = min(height, len(text))
+        for i in range(nlines):
+            self.parent.scr.addstr(y + i, x, text[i])
--- test-curses.py
+++ test-curses.py
@@ -1,106 +1,21 @@
-import textwrap
+from src import tui
 
 import curses
 
 
-class Rect:
-    def __init__(self, x:int=0, y:int=0, widht:int=0, height:int=0):
-        self.x = x
-        self.y = y
-        self.w = widht
-        self.h = height
+root = tui.Tui()
 
-    def __str__(self) -> str:
-        return f"Rect({self.x}, {self.y}, {self.w}, {self.h})"
+label_long = tui.Label(root)
+label_long.text = "Hello, World! I am a very long text that overflows from its boundaries."
+label_long.grid(row=0, height=2)
 
-    def __repr__(self) -> str:
-        return str(self)
+label_short = tui.Label(root)
+label_short.text = "Hello, World!"
+label_short.grid(row=1, height=1)
 
-    # Properties
+label = tui.Label(root)
+label.text = "Plop hehe"
+label.grid(row=2)
 
-    @property
-    def y_max(self) -> int:
-        return self.y + self.h - 1
-
-    @property
-    def x_max(self) -> int:
-        return self.x + self.w - 1
-
-
-class Widget:
-    def __init__(self, parent, rect:Rect, pair:int=1):
-        self.parent = parent
-        self.rect = rect
-        self.pair = pair
-
-    def fill(self, char:str=" "):
-        line = char * self.rect.w
-        for row in range(self.rect.h):
-            self.parent.addstr(self.rect.y + row, self.rect.x, line, curses.color_pair(self.pair))
-
-    def addstr(self, y:int, x:int, string:str):
-        self.parent.addstr(y, x, string, curses.color_pair(self.pair))
-
-
-class Frame(Widget):
-    def __init__(self, parent, rect:Rect, pair:int=1):
-        super().__init__(parent, rect, pair)
-
-    def border(self):
-        self.fill()
-        hline = "+" + "-" * (self.rect.w - 2) + "+"
-        self.addstr(self.rect.y, self.rect.x, hline)
-        for row in range(self.rect.y + 1, self.rect.y_max):
-            self.addstr(row, self.rect.x, "|")
-            self.addstr(row, self.rect.x_max, "|")
-        self.addstr(self.rect.y_max, self.rect.x, hline)
-
-
-class Label(Widget):
-    # FIXME: How to manage text overflow?
-    def __init__(self, parent, rect:Rect, pair:int=1, text:str=""):
-        super().__init__(parent, rect, pair)
-        self._text = text
-
-    # Properties
-
-    @property
-    def text(self) -> str:
-        return self._text
-
-    @text.setter
-    def text(self, value:str):
-        self._text = value
-        self.fill()
-
-        text = textwrap.wrap(self._text, self.rect.w)
-        nlines = min(self.rect.h, len(text))
-        for i in range(nlines):
-            self.addstr(self.rect.y + i, self.rect.x, text[i])
-
-
-
-def main():
-    scr = curses.initscr()
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    curses.init_pair(2, curses.COLOR_WHITE, curses.COLOR_BLUE)
-    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_RED)
-    curses.init_pair(4, curses.COLOR_WHITE, curses.COLOR_GREEN)
-    widget = Frame(scr, Rect(0, 0, 30, 10), 2)
-    widget.fill()
-    frame = Frame(scr, Rect(0, 11, 30, 10), 3)
-    frame.border()
-    frame = Frame(scr, Rect(32, 11, 30, 10), 4)
-    frame.fill()
-    label = Label(scr, Rect(32, 0, 30, 1), 2)
-    label.text = "Hello, World!"
-    label = Label(scr, Rect(32, 1, 30, 2), 1)
-    label.text = "How are you today?"
-    label.text = "Extra large text that overflow from its boundaries"
-    scr.refresh()
-    scr.getkey()
-
-
-if __name__ == "__main__":
-    main()
+root.update()
+root.scr.getch()
