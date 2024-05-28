cd826d762a5aca3e5a87a3e30dce49a2464fdb40
deplanty <yvon.deplante@gmail.com>
2024-05-28 12:07:00 +0200

    Test the new Choice widget

--- test.py
+++ test.py
@@ -1,14 +1,28 @@
+import curses
+
 from src import tui
 
 
 root = tui.Tui()
+label = tui.Label(root)
+label.text = "Select 'Plop' or write 'exit' in the entry.\n"
+label.text += "Use TAB to navigate through the widgets."
+label.pack()
+
+tui.Frame(root).pack()
 
 choice = tui.Choice(root)
-choice.add_elements("Salut", "Plop", "Choix 3", "Choix 4")
+choice.add_elements("Choix 1", "Choix 2", "Choix 3", "Choix 4", "Quit")
 choice.pack()
 choice.focus_set()
 
+entry = tui.Entry(root)
+entry.pack()
+
+choice.focus_next = entry
+entry.focus_next = choice
+
 while True:
     root.update()
-    if choice.choice == "Plop":
-        break
+    if choice.choice == "Quit" or entry.text == "exit":
+        break   
