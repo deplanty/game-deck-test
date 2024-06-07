a19e791cd362d746148c5757395ddc381a4f9537
deplanty <yvon.deplante@gmail.com>
2024-06-07 14:03:48 +0200

    Differenciate normal frames and frames with border and title

--- src/tui/__init__.py
+++ src/tui/__init__.py
@@ -4,6 +4,7 @@ from .widget import Widget
 from .tui import Tui
 from .frame import Frame
 from .label import Label
+from .label_frame import LabelFrame
 from .entry import Entry
 from .progressbar import Progressbar
 from .choice import Choice
--- src/tui/label_frame.py
+++ src/tui/label_frame.py
@@ -1,16 +1,34 @@
+import curses
+
 from src.tui import Widget
+from src.tui.style import BoxClean, Style
 
 
-class Frame(Widget):
+class LabelFrame(Widget):
     """
     A simple widget use to contains other widgets
 
     Args:
         parent (Widget): The parent widget
+        border (bool): This frame have borders or not. TODO: WIP
     """
 
-    def __init__(self, parent:Widget):
+    style_border = BoxClean
+
+    def __init__(self, parent, text:str=""):
         super().__init__(parent)
+
+        self.text = text
+        self.pad_intern.x = 1
+        self.pad_intern.y = 1
+
+    @property
+    def text(self) -> str:
+        return self._text
+
+    @text.setter
+    def text(self, title:str):
+        self._text = title
 
     @property
     def height_calc(self) -> int:
@@ -24,7 +42,7 @@ class Frame(Widget):
         """
 
         if not self.children:
-            return 1
+            return self.pad_intern.y * 2
 
         layout = self.children[0]._layout
         if layout == "grid":
@@ -36,15 +54,30 @@ class Frame(Widget):
 
             height = 0
             for children in rows.values():
+                # Here, we use height_calc to avoid infinite recursion
                 height += max(child.height_calc for child in children)
-            return height
+            return height + self.pad_intern.y * 2
 
         elif layout == "pack":
             h = sum(child.height for child in self.children)
             return self.pad_intern.y * 2 + h
-
         elif layout == "place":
             return self.pad_intern. y * 2
 
     def update(self):
-        pass
+        # The string to display
+        line = self.style_border.H * (self.width - 2)
+        top = f"{self.style_border.TL}{line}{self.style_border.TR}"
+        bottom = f"{self.style_border.BL}{line}{self.style_border.BR}"
+
+        # Display the strings with the correct style
+        self.set_style(Style.TEXT_MUTED)
+        self.addstr(self.y, self.x, top)
+        row = 0
+        for row in range(1, self.height - 1):
+            self.addstr(self.y + row, self.x, self.style_border.V)
+            self.addstr(self.y + row, self.x + self.width - 1, self.style_border.V)
+        self.addstr(self.y + row + 1, self.x, bottom)
+
+        self.set_style(Style.NORMAL)
+        self.addstr(self.y, self.x + 1, self.text)
