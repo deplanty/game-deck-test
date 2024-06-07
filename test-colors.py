7ed4f3e2f60c41b03700c6334f86d27609ea54be
deplanty <yvon.deplante@gmail.com>
2024-06-07 18:34:31 +0200

    Change how the curses colors are displayed with their corresponding ID

--- test-colors.py
+++ test-colors.py
@@ -7,23 +7,34 @@ def main(stdscr):
     for i in range(1, n_colors):
         curses.init_pair(i, -1, i)
     try:
-        for i in range(256):
-            stdscr.addstr(str(i), curses.color_pair(i))
+        for i in range(16):
+            stdscr.addstr(f"{i:^4}", curses.color_pair(i))
+        stdscr.move(1, 0)
+        for i in range(16, 16 + 6*6*6):
+            stdscr.addstr(f"{i:^4}", curses.color_pair(i))
+        stdscr.move(7, 0)
+        for i in range(16 + 6*6*6, 16 + 6*6*6 + 24):
+            stdscr.addstr(f"{i:^4}", curses.color_pair(i))
 
         col = 0
-        row = 7
+        row = 9
         stdscr.move(row, 0)
+        stdscr.addstr(f"{col + 1:3d} ")
         for i in range(15):
             col += 1
             stdscr.addstr("  ", curses.color_pair(col))
-        for i in range(36):
+        for i in range(6):
             row += 1
             stdscr.move(row, 0)
+            stdscr.addstr(f"{col + 1:3d} ")
             for j in range(6):
-                col += 1
-                stdscr.addstr("  ", curses.color_pair(col))
+                for k in range(6):
+                    col += 1
+                    stdscr.addstr("  ", curses.color_pair(col))
+                stdscr.addstr(" ")
         row += 1
         stdscr.move(row, 0)
+        stdscr.addstr(f"{col + 1:3d} ")
         for i in range(24):
             col += 1
             stdscr.addstr("  ", curses.color_pair(col))
