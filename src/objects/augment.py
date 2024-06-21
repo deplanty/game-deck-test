a7c8c3905cb3e97e4499fab9d90bdcee98fc6e20
deplanty <yvon.deplante@gmail.com>
2024-06-21 18:16:47 +0200

    Add a description to the augment

--- src/objects/augment.py
+++ src/objects/augment.py
@@ -7,6 +7,7 @@ class Augment:
 
         self.name = ""
         self.trigger = ""
+        self.description = ""
         self.strenght = 0
         self.resistance = 0
 
@@ -16,11 +17,15 @@ class Augment:
     def __repr__(self) -> str:
         return str(self)
 
+    @property
+    def info(self) -> str:
+        return self.description.format(augment=self)
+
+    def copy(self):
+        return copy.deepcopy(self)
+
     @classmethod
     def from_dict(cls, iid:int, data:dict):
         augment = cls(iid)
         augment.__dict__.update(data)
         return augment
-
-    def copy(self):
-        return copy.deepcopy(self)
