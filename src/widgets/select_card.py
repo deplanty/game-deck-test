0313e12841a98ed30c356bd5c058434be0407e40
deplanty <yvon.deplante@gmail.com>
2024-06-27 11:22:02 +0200

    Show the details of the currently selected card

--- src/widgets/select_card.py
+++ src/widgets/select_card.py
@@ -6,7 +6,9 @@ class SelectCardUi:
         self.parent = parent
 
         self.choice_card = tui.Choice(parent)
-        self.choice_card.pack(fill=True)
+        self.choice_card.grid(0, 0)
+        self.label_card = tui.Label(parent, prefix="Current card:\n")
+        self.label_card.grid(0, 1)
 
         # Initialize the values
         for card in parent.cards:
@@ -23,6 +25,7 @@ class SelectCard(tui.LabelFrame):
         self.selected = tui.Signal()
         self.ui = SelectCardUi(self)
         self.ui.choice_card.selected.connect(self._on_choice_card_selected)
+        self.ui.choice_card.hovered.connect(self._on_choice_card_hovered)
 
     @property
     def card(self):
@@ -33,3 +36,7 @@ class SelectCard(tui.LabelFrame):
 
     def _on_choice_card_selected(self):
         self.selected.emit()
+
+    def _on_choice_card_hovered(self):
+        self.ui.label_card.text = self.card.info_full
+        self.ui.label_card.update()
